88dc7432de786a77582167d20618b8bfee8eb393
RitaShkodra <rs67817@ubt-uni.net>
2024-02-01 11:27:18 +0100

    fixed login and loginLogic, now u can log in as admin or user, added sql

--- missskin.sql
+++ missskin.sql
@@ -3,82 +3,89 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 24, 2024 at 04:06 PM
+-- Generation Time: Feb 01, 2024 at 11:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
--- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
--- START TRANSACTION;
--- SET time_zone = "+00:00";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
--- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
--- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
--- /*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- --
--- -- Database: `missskin`
--- --
+--
+-- Database: `missskin`
+--
 
--- -- --------------------------------------------------------
+-- --------------------------------------------------------
 
--- --
--- -- Table structure for table `home`
--- --
+--
+-- Table structure for table `home`
+--
 
--- CREATE TABLE `home` (
---   `ID` int(11) NOT NULL,
---   `home_image` varchar(255) NOT NULL,
---   `home_emri` varchar(255) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `home` (
+  `ID` int(11) NOT NULL,
+  `home_image` varchar(255) NOT NULL,
+  `home_emri` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- -- --------------------------------------------------------
+-- --------------------------------------------------------
 
--- --
--- -- Table structure for table `user`
--- --
+--
+-- Table structure for table `user`
+--
 
--- CREATE TABLE `user` (
---   `id` int(11) NOT NULL,
---   `Full Name` varchar(255) NOT NULL,
---   `Username` varchar(255) NOT NULL,
---   `Email` varchar(255) NOT NULL,
---   `Password` varchar(20) NOT NULL
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `userpassword` varbinary(255) NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --
--- -- Indexes for dumped tables
--- --
+--
+-- Dumping data for table `user`
+--
 
--- --
--- -- Indexes for table `home`
--- --
--- ALTER TABLE `home`
---   ADD PRIMARY KEY (`ID`);
+INSERT INTO `user` (`userID`, `username`, `email`, `userpassword`, `role`) VALUES
+(37, 'ritash', 'ritasgg@gmail.com', 0x24327924313024697650646961485a6f3631386343772e624f506c414f386b62686a5559557a7151657142397270453136754d396139516534435775, 1);
 
--- --
--- -- Indexes for table `user`
--- --
--- ALTER TABLE `user`
---   ADD PRIMARY KEY (`id`);
+--
+-- Indexes for dumped tables
+--
 
--- --
--- -- AUTO_INCREMENT for dumped tables
--- --
+--
+-- Indexes for table `home`
+--
+ALTER TABLE `home`
+  ADD PRIMARY KEY (`ID`);
 
--- --
--- -- AUTO_INCREMENT for table `home`
--- --
--- ALTER TABLE `home`
---   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`);
 
--- --
--- -- AUTO_INCREMENT for table `user`
--- --
--- ALTER TABLE `user`
---   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
--- COMMIT;
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `home`
+--
+ALTER TABLE `home`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
